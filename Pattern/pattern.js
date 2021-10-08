dc7bc46aa252ed10cb81dcc816470d37601b34cc
krishna <krishnajj99@gmail.com>
2021-10-08 13:20:07 +0530

    pattern functionality done

---
+++ Pattern/pattern.js
@@ -0,0 +1,10 @@
+var num;
+var no=prompt("Please provide a number for the no of rows to be print in a pattern...");
+for(var m=1;m<=no;m++)
+{
+for(var n=1;n<=m;n++)
+{
+document.write("0"+n+" ");
+}
+document.write("<br />");
+}
